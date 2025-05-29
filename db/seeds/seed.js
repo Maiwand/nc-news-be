e0ddc7485d08c67901403cef148f66b383adc09a
Maiwand Ayubi <maiiiwand@gmail.com>
2025-05-29 16:55:48 +0100

    completed: seed function

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,108 @@
-const db = require("../connection")
+const db = require("../connection");
+const format = require("pg-format");
+const {
+  convertTimestampToDate,
+  createArticleRef,
+  formatComments,
+} = require("./utils");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  await db.query(`DROP TABLE IF EXISTS comments, articles, users, topics;`);
+
+  await db.query(`
+    CREATE TABLE topics (
+      slug VARCHAR PRIMARY KEY,
+      description VARCHAR NOT NULL,
+      img_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE users (
+      username VARCHAR PRIMARY KEY,
+      name VARCHAR NOT NULL,
+      avatar_url VARCHAR (1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR NOT NULL,
+      topic VARCHAR REFERENCES topics(slug),
+      author VARCHAR REFERENCES users(username),
+      body TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR (1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      author VARCHAR REFERENCES users(username),
+      article_id INT REFERENCES articles(article_id),
+      votes INT DEFAULT 0,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      body TEXT NOT NULL
+    );
+  `);
+
+  const topicInsertQuery = format(
+    `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *;`,
+    topicData.map(({ slug, description, img_url }) => [
+      slug,
+      description,
+      img_url,
+    ])
+  );
+  await db.query(topicInsertQuery);
+
+  const userInsertQuery = format(
+    `INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *;`,
+    userData.map(({ username, name, avatar_url }) => [
+      username,
+      name,
+      avatar_url,
+    ])
+  );
+  await db.query(userInsertQuery);
+
+  const formattedArticles = articleData.map(convertTimestampToDate);
+
+  const articleInsertQuery = format(
+    `INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *;`,
+    formattedArticles.map(
+      ({ title, topic, author, body, created_at, votes, article_img_url }) => [
+        title,
+        topic,
+        author,
+        body,
+        created_at,
+        votes,
+        article_img_url,
+      ]
+    )
+  );
+  const { rows: insertedArticles } = await db.query(articleInsertQuery);
+
+  const articleRef = createArticleRef(insertedArticles);
+  const formattedComments = commentData
+    .map(convertTimestampToDate)
+    .map((comment) => formatComments([comment], articleRef)[0]);
+
+  const commentInsertQuery = format(
+    `INSERT INTO comments (author, article_id, votes, created_at, body) VALUES %L RETURNING *;`,
+    formattedComments.map(({ author, article_id, votes, created_at, body }) => [
+      author,
+      article_id,
+      votes,
+      created_at,
+      body,
+    ])
+  );
+  await db.query(commentInsertQuery);
 };
+
 module.exports = seed;
