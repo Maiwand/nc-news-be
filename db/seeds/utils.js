6a6a0ec09ee728d3ed5951bdac8ffe30afef01e9
Maiwand Ayubi <maiiiwand@gmail.com>
2025-05-29 16:55:16 +0100

    completed: createArticleRef and formatComment

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -5,5 +5,20 @@ exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
+exports.createArticleRef = (articles) => {
+  const ref = {};
+  articles.forEach(({ title, article_id }) => {
+    ref[title] = article_id;
+  });
+  return ref;
+};
 
-
+exports.formatComments = (comments, articleRef) => {
+  return comments.map(({ created_by, belongs_to, ...rest }) => {
+    return {
+      author: created_by,
+      article_id: articleRef[belongs_to],
+      ...rest,
+    };
+  });
+};
