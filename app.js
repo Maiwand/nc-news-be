949b4eba2430c413a7b54f0ba9c57c6548001dd2
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-04 16:17:56 +0100

    Add GET /api/articles route

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const endpoints = require("./endpoints.json");
 const { getTopics } = require("./controllers/topics.controller");
+const { getAllArticles } = require("./controllers/articles.controller");
 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.status(200).send({ endpoints });
+app.get("/api", (request, response) => {
+  response.status(200).send({ endpoints });
 });
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles", getAllArticles);
 
 module.exports = app;
