9345e553808c53e24d8e9282bd0c500b70c20889
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-04 16:43:29 +0100

    Add GET /api/users route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const endpoints = require("./endpoints.json");
 const { getTopics } = require("./controllers/topics.controller");
 const { getAllArticles } = require("./controllers/articles.controller");
+const { getAllUsers } = require("./controllers/users.controller");
 
 app.use(express.json());
 
@@ -13,5 +14,7 @@ app.get("/api", (request, response) => {
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getAllArticles);
+
+app.get("/api/users", getAllUsers);
 
 module.exports = app;
