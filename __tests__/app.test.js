8f22c91c3610124e462f33613e76397ada1072ee
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-04 16:02:23 +0100

    Add test for GET /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,42 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+
 const request = require("supertest");
 const app = require("../app");
-/* Set up your beforeEach & afterAll functions here */
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
 
 describe("GET /api", () => {
-  test("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("GET - 200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("/api/topics", () => {
+  test("GET - 200: Responds with an array of all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.topics)).toBe(true);
+        expect(body.topics.length).toBeGreaterThan(0);
+        body.topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
       });
   });
 });
