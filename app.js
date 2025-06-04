77d3de6c87f6bf1af3c57b1de6f11a3ea9e60bb4
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-04 15:58:07 +0100

    Add GET /api/topics endpoint to serve all topics

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const endpoints = require("./endpoints.json");
+const { getTopics } = require("./controllers/topics.controller");
+
+app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.status(200).send({ endpoints });
 });
+
+app.get("/api/topics", getTopics);
 
 module.exports = app;
