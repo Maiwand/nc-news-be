7c732d679bd5abc6b50ad359db5055a1b71f5d86
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-05 13:44:08 +0100

    Add model function to fetch an article by ID with 404 handling

--- models/articles.model.js
+++ models/articles.model.js
@@ -13,4 +13,19 @@ const selectAllArticles = () => {
   return db.query(queryString).then(({ rows }) => rows);
 };
 
-module.exports = { selectAllArticles };
+const selectArticleById = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .then(({ rows }) => {
+      const article = rows[0];
+      if (!article) {
+        return Promise.reject({
+          status: 404,
+          msg: `Article ${article_id} not found`,
+        });
+      }
+      return rows[0];
+    });
+};
+
+module.exports = { selectAllArticles, selectArticleById };
