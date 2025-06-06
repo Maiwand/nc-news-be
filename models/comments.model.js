308fb6811af5d119095bcd86a800d434b865936d
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-06 10:49:08 +0100

    Add model function to insert a comment by article_id

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const articles = require("../db/data/test-data/articles");
 
 const selectCommentsByArticleId = (article_id) => {
   return db
@@ -16,4 +17,42 @@ const selectCommentsByArticleId = (article_id) => {
     });
 };
 
-module.exports = { selectCommentsByArticleId };
+const insertCommentByArticleId = (article_id, newComment) => {
+  const { username, body } = newComment;
+  if (!username || !body) {
+    return Promise.reject({ status: 400, msg: "Missing required fields" });
+  }
+
+  const checkArticleExists = db.query(
+    `SELECT * FROM articles WHERE article_id = $1`,
+    [article_id]
+  );
+
+  const checkUserExists = db.query(`SELECT * FROM users WHERE username = $1`, [
+    username,
+  ]);
+
+  return Promise.all([checkArticleExists, checkUserExists]).then(
+    ([checkArticleExistsResolved, checkUserExistsResolved]) => {
+      if (checkArticleExistsResolved.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      if (checkUserExistsResolved.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "User not found" });
+      }
+
+      return db
+        .query(
+          `
+        INSERT INTO comments (author, article_id, body)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+        `,
+          [username, article_id, body]
+        )
+        .then(({ rows }) => rows[0]);
+    }
+  );
+};
+
+module.exports = { selectCommentsByArticleId, insertCommentByArticleId };
