a53a4c4b6ecce6e336f084f9c18d31402ff9c47a
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-06 16:09:01 +0100

    Add controller to update article vote by article_id

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -2,6 +2,8 @@ const { request, response } = require("../app");
 const {
   selectAllArticles,
   selectArticleById,
+  updateArticleById,
+  checkArticleExists,
 } = require("../models/articles.model");
 
 const getAllArticles = (request, response) => {
@@ -19,4 +21,22 @@ const getArticleById = (request, response, next) => {
     .catch(next);
 };
 
-module.exports = { getAllArticles, getArticleById };
+const patchArticleById = (request, response, next) => {
+  const { article_id } = request.params;
+  const { inc_votes } = request.body;
+
+  if (typeof inc_votes !== "number") {
+    return next({ status: 400, msg: "Invalid inc_votes format" });
+  }
+
+  Promise.all([
+    checkArticleExists(article_id),
+    updateArticleById(article_id, inc_votes),
+  ])
+    .then(([, updatedArticle]) => {
+      response.status(200).send({ article: updatedArticle });
+    })
+    .catch(next);
+};
+
+module.exports = { getAllArticles, getArticleById, patchArticleById };
