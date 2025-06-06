b3ac72d800ba2064cf8ba82910a5d6e456d1635f
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-06 16:05:20 +0100

    Refactor: checkArticleExist to resolve promise internally in case of a no found

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,7 +1,13 @@
 const db = require("../db/connection");
 
 const checkArticleExists = (article_id) => {
-  return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id]);
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+    });
 };
 
 const selectAllArticles = () => {
