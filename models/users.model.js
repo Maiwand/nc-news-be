6c991bc3b01c68b5cfce534e1f51c6910d53e079
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-06 16:06:05 +0100

    Refactor: checkUserExist to resolve promise internally in case of a no found

--- models/users.model.js
+++ models/users.model.js
@@ -1,7 +1,13 @@
 const db = require("../db/connection");
 
 const checkUserExists = (username) => {
-  return db.query(`SELECT * FROM users WHERE username = $1`, [username]);
+  return db
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "User not found" });
+      }
+    });
 };
 
 const selectAllUsers = () => {
