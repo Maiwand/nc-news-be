22f856dca7687360c7a8fa4a30f97336c51a2c6c
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-06 16:08:28 +0100

    Add model function to update article vote by article_id

--- models/articles.model.js
+++ models/articles.model.js
@@ -38,8 +38,28 @@ const selectArticleById = (article_id) => {
     });
 };
 
+const updateArticleById = (article_id, inc_votes) => {
+  return db
+    .query(
+      `
+    UPDATE articles
+    SET votes = votes + $1
+    WHERE article_id = $2
+    RETURNING *;
+    `,
+      [inc_votes, article_id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      return rows[0];
+    });
+};
+
 module.exports = {
   selectAllArticles,
   selectArticleById,
+  updateArticleById,
   checkArticleExists,
 };
