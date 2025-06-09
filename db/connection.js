3c77920e7613f64e7b8dea49a70d9966218d1630
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-09 15:28:25 +0100

    fixed typo

--- db/connection.js
+++ db/connection.js
@@ -4,17 +4,17 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
 const config = {};
 
-const db = new Pool();
-
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
 }
+
+const db = new Pool(config);
 
 module.exports = db;
