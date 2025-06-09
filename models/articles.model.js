6d3eb9f2af9382b7fa6099b1c371e570419d328f
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-09 14:22:21 +0100

    Add comment_count to GET /api/articles/:article_id response

--- models/articles.model.js
+++ models/articles.model.js
@@ -53,7 +53,16 @@ const selectAllArticles = (sort_by = "created_at", order = "desc", topic) => {
 
 const selectArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .query(
+      `
+      SELECT articles.*, 
+      COUNT(comments.comment_id)::INT AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;`,
+      [article_id]
+    )
     .then(({ rows }) => {
       const article = rows[0];
       if (!article) {
