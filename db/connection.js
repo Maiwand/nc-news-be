2dfc8d729ae76a785a1355019615d79b2cb1a4ac
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-09 14:59:51 +0100

    Updated seeding for production

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,6 @@
 const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || "development";
-
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -13,6 +9,8 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 }
 
 const config = {};
+
+const db = new Pool();
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
