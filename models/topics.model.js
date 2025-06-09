b8fc571fa57e774d9508f7a73be0a59630dec4a7
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-09 12:29:40 +0100

    Add checkTopicExists function

--- models/topics.model.js
+++ models/topics.model.js
@@ -6,4 +6,15 @@ const selectTopics = () => {
   });
 };
 
-module.exports = { selectTopics };
+const checkTopicExists = (topic) => {
+  if (!topic) return Promise.resolve;
+  return db
+    .query(`SELECT * FROM topics WHERE slug = $1`, [topic])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Topic not found" });
+      }
+    });
+};
+
+module.exports = { selectTopics, checkTopicExists };
