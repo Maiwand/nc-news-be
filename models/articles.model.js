1bb8f2ea1190d4468705700eb1d8c4a93cccb308
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-10 15:29:12 +0100

    Add feature to checkArticleExist to return the article if needed

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,11 +1,14 @@
 const db = require("../db/connection");
 
-const checkArticleExists = (article_id) => {
+const checkArticleExists = (article_id, returnArticleData = false) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      if (returnArticleData) {
+        return rows[0];
       }
     });
 };
